6372fe2c6b5da391b7e2fa03b318338d7a28776c
zhangdong <zhangdong@tedu.cn>
2017-11-07 17:56:46 +0800

    2017/11/07

---
+++ dairy_info.sql
@@ -0,0 +1,190 @@
+drop table if exists dairy_info;
+CREATE TABLE dairy_info(
+  dairy_id INT PRIMARY KEY AUTO_INCREMENT,
+  days INT,              #术后天数
+  after_pic VARCHAR(255),         #术后照片路径
+  before_pic  VARCHAR(255),      #术前照片路径
+  project VARCHAR(255),        #项目
+  doctor VARCHAR(255),           # 医生
+  hospital VARCHAR(255)          #医院
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+73,
+'img/picture2/20171017133207891_240_320.jpg',
+'img/picture2/20171009182133801_240_360.jpg',
+'吸脂瘦大腿',
+'孙笠',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+62,
+'img/picture2/20171031163648597_240_324.jpg',
+'img/picture2/20170820235200881_240_160.jpg',
+'鼻综合',
+'雷雪华',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+57,
+'img/picture2/20171104171401934_240_240.jpg',
+'img/picture2/20170916210353872_240_160.jpg',
+'切开双眼皮',
+'郑静伟',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+57,
+'img/picture2/20171104171401934_240_240.jpg',
+'img/picture2/20170916210353872_240_160.jpg',
+'切开双眼皮',
+'郑静伟',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+63,
+'img/picture2/20171014165317746_240_320.jpg',
+'img/picture2/20170906171720619_240_208.jpg',
+'自体脂肪丰额头',
+'孙笠',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+91,
+'img/picture2/20171106101251142_240_320.jpg',
+'img/picture2/20170913162155156_240_180.jpg',
+'眼部综合',
+'郑静伟',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+104,
+'img/picture2/20170906185008302_240_320.jpg',
+'img/picture2/20170525150912973_240_320.jpg',
+'种植发际线',
+'谷远飞',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+66,
+'img/picture2/20171104095548120_240_320.jpg',
+'img/picture2/20170905110651992_240_320.jpg',
+'切开双眼皮',
+'张军辉',
+'长沙南湖整形医疗美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+73,
+'img/picture2/20171104102339840_240_427.jpg',
+'img/picture2/20170823102214215_240_160.jpg',
+'膨体隆鼻',
+'何怀万',
+'长沙南湖整形医疗美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+309,
+'img/picture2/20171005112644244_240_138.png',
+'img/picture2/20170825151308254_240_270.jpg',
+'假体隆胸',
+'孙笠',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+70,
+'img/picture2/20171101141348161_240_427.jpg',
+'img/picture2/20170904101221557_240_160.jpg',
+'鼻综合',
+'孙笠',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+140,
+'img/picture2/20171031180045307_240_320.jpg',
+'img/picture2/20170805142231469_240_255.jpg',
+'假体隆胸',
+'谢卫国',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+160,
+'img/picture2/20171016144755335_240_320.jpg',
+'img/picture2/20170812154137749_240_233.jpg',
+'硅胶垫下巴',
+'张朋',
+'上海华美医疗美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+43,
+'img/picture2/20171009155915630_240_320.jpg',
+'img/picture2/20170909153250417_240_320.jpg',
+'眼部综合',
+'郑静伟',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+268,
+'img/picture2/20160708103223703_240_321.jpg',
+'img/picture2/20160206165552231_240_240.jpg',
+'切开双眼皮',
+'郑静伟',
+'上海百达丽医疗美容门诊部'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+16,
+'img/picture2/20170922220503801_240_240.jpg',
+'img/picture2/20170906212253587_240_240.jpg',
+'自体脂肪丰额头',
+'别筝',
+'上海百达丽医疗美容门诊部'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+75,
+'img/picture2/20171101165545839_240_320.jpg',
+'img/picture2/20171101161222675_240_225.png',
+'鼻部修复',
+'雷雪华',
+'福州格莱美美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+110,
+'img/picture2/20171104100751244_240_320.jpg',
+'img/picture2/20170717150129676_240_160.jpg',
+'鼻综合',
+'何怀万',
+'长沙南湖整形医疗美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+153,
+'img/picture2/20171030162603618_240_240.jpg',
+'img/picture2/20170916133153872_240_160.jpg',
+'鼻综合',
+'何怀万',
+'长沙南湖整形医疗美容医院'
+);
+insert into dairy_info (days,after_pic,before_pic,project,doctor,hospital)
+values (
+43,
+'img/picture2/20171022150312292_240_135.jpg',
+'img/picture2/20170907182932658_240_320.jpg',
+'玻尿酸丰太阳穴',
+'曹泓',
+'福州格莱美美容医院'
+);
