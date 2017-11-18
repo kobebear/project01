54967e4f8f6d516f9cb93ec27aeffc5aaf00f483
zhangdong <zhangdong@tedu.cn>
2017-11-18 12:23:23 +0800

    2017/11/07

--- JS/dairy.js
+++ JS/dairy.js
@@ -1,12 +1,22 @@
+//加载尾部
+(()=>{
+    ajax("get","indexFooter.html","","text").then(html=>{
+        document.getElementById("footer").innerHTML=html;
+    });
+
+})();
+
+//加载条目和分页
 $(function(){
     function loadProductByPage(pno,pageSize){
         $.ajax({
             type:"GET",
-            url:"data/diary/diaryinfo.php",
+            url:"../data/diary/diaryInfo.php",
             success:function(data){
-                var html="";
-                for(var i of data ){
-                    html+=`
+                // let fhtml="";
+                console.log(data);
+                for(let i of data){
+                    fhtml+=`
                     <li>
                         <p class="diary-t">
                             <a href="#">${i.title}</a>
@@ -19,40 +29,42 @@ $(function(){
                     </li>
                     `;
                 }
-                $("#diary-InfoC").html(html);
-
-                var html = "";
-                if(data.pno-2>0){
-                    html += `
-     <li><a href="#">${data.pno-2}</a></li>
-     `;
-                }
-                if(data.pno-1>0){
-                    html += `
-     <li><a href="#">${data.pno-1}</a></li>
-     `;
-                }
-
-                html += `
-     <li class="active"><a href="#">${data.pno}</a></li>
-     `;
-                if(data.pno+1<=data.pageCount){
-                    html += `
-     <li><a href="#">${data.pno+1}</a></li>
-     `;
-                }
-                if(data.pno+2<=data.pageCount){
-                    html += `
-     <li><a href="#">${data.pno+2}</a></li>
-     `;
-                }
-                $("#pagination").html(html);
+                $("#diary-InfoC").html(fhtml);
+     //
+     //            let html = "";
+     //            if(data.pno-2>0){
+     //                html += `
+     // <li><a href="#">${data.pno-2}</a></li>
+     // `;
+     //            }
+     //            if(data.pno-1>0){
+     //                html += `
+     // <li><a href="#">${data.pno-1}</a></li>
+     // `;
+     //            }
+     //
+     //            html += `
+     // <li class="active"><a href="#">${data.pno}</a></li>
+     // `;
+     //            if(data.pno+1<=data.pageCount){
+     //                html += `
+     // <li><a href="#">${data.pno+1}</a></li>
+     // `;
+     //            }
+     //            if(data.pno+2<=data.pageCount){
+     //                html += `
+     // <li><a href="#">${data.pno+2}</a></li>
+     // `;
+     //            }
+     //            $("#pagination").html(html);
             },
             error:function(){
                 alert("网络故障，请检查");
             }
-        });
-        //7:拼接计算
-        loadProductByPage(1,8);
+        }).then(data=>{
+            console.log(data);
+        })
     }
-})
+    //7:拼接计算
+    loadProductByPage(1,8);
+});
