4fe6a4ec60be7a8377a9f9ee3d2a492a9818b00e
Dong Zhang <zhangdong@tedu.cn>
2017-11-24 13:00:23 +0800

    默认

--- data/index/realcase.php
+++ data/index/realcase.php
@@ -4,7 +4,7 @@ header("Content-Type:application/json;charset=utf-8");
 require_once("../init.php");
 
 mysqli_query($conn,"SET NAMES UTF8");
-$sql="select bm,sm from case_pic";
+$sql="select * from case_pic";
 $result=sql_execute($sql);
 
 echo json_encode($result);
